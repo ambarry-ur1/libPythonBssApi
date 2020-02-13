de7802fe0d4a2a7940adeeb2ac1564fad480b5f8
ambarry <amadou-bobo.barry@univ-rennes1.fr>
2020-02-13 17:00:04 +0100

    Ajout de 11 cas de tests d'intégration sur les ressources

--- test_integration/lib_Partage_BSS/services/test_ServiceResource.py
+++ test_integration/lib_Partage_BSS/services/test_ServiceResource.py
@@ -18,12 +18,13 @@ def delete_resource(name):
 def test_cleanup_bss_environment(test_config):
     print("Cleanup BSS environment before running tests...")
     #create_account(test_config['accountname'])
-    #delete_resource(test_config['groupname'])
+    delete_resource(test_config['groupname'])
 
 def test_createResource_cas_normal(test_config):
     newResource = ResourceService.createResource(test_config['groupname'],test_config['userPassword'],test_config['zimbraCalResType'],test_config['displayName'],password=None)
     resource = ResourceService.getResource(test_config['groupname'])
     assert resource.name == test_config['groupname']
+
 
 def test_createResource_cas_resourceExistant(test_config):
     with pytest.raises(ServiceException):
@@ -37,3 +38,54 @@ def test_getResource_cas_resource_inexistant(test_config):
     resource = ResourceService.getResource("inexistant" + '@' + test_config['bss_domain'])
     assert resource == None
 
+def test_modifyResource_cas_Normal(test_config):
+    resource_as_dict = { 'displayName': "TestName",
+                        'postalCode': "00000555",
+                        'zimbraCalResAutoDeclineIfBusy': "FALSE",
+                        'zimbraCalResContactEmail': "contact@x.fr",
+                        'l': "Rennes"
+                        }
+    resource = ResourceService.getResource(test_config['groupname'])
+    for attribute in resource_as_dict:
+        setattr(resource, "_" + attribute, resource_as_dict[attribute])
+    ResourceService.modifyResource(resource)
+    resource = ResourceService.getResource(test_config['groupname'])
+    errors = 0
+    for attribute in resource_as_dict:
+        if getattr(resource, "_" + attribute) != resource_as_dict[attribute]:
+            errors = errors + 1
+    assert errors == 0
+
+def test_modifyResource_cas_addZimbraPrefCalendarForwardInvitesTo(test_config):
+    resource = ResourceService.getResource(test_config['groupname'])
+    resource.addZimbraPrefCalendarForwardInvitesTo("resource@x.fr")
+    ResourceService.modifyResource(resource)
+    resource = ResourceService.getResource(test_config['groupname'])
+
+
+    assert "resource@x.fr" in resource.zimbraPrefCalendarForwardInvitesTo
+
+
+def test_modifyResource_cas_addZimbraPrefCalendarForwardInvitesToCompteErrone(test_config):
+    resource = ResourceService.getResource(test_config['groupname'])
+    resource.addZimbraPrefCalendarForwardInvitesTo("resourcettx.fr")
+    ResourceService.modifyResource(resource)
+    resource = ResourceService.getResource(test_config['groupname'])
+
+    assert "resourcettx.fr" not in resource.zimbraPrefCalendarForwardInvitesTo
+
+def test_modifyResource_cas_removeZimbraPrefCalendarForwardInvitesTo(test_config):
+    resource = ResourceService.getResource(test_config['groupname'])
+    resource.removeZimbraPrefCalendarForwardInvitesTo("resource@x.fr")
+    ResourceService.modifyResource(resource)
+    resource1 = ResourceService.getResource(test_config['groupname'])
+    assert "resource@x.fr" not in resource1.zimbraPrefCalendarForwardInvitesTo
+
+def test_deleteResource_cas_normal(test_config):
+    newResource = ResourceService.deleteResource(test_config['groupname'])
+    resource = ResourceService.getResource(test_config['groupname'])
+    assert resource == None
+
+def test_deleteResource_cas_resource_inexistant(test_config):
+    with pytest.raises(ServiceException):
+        resource = ResourceService.deleteResource(test_config['groupname'])
